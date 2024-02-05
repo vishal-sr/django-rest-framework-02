1e57cf3edc2889659cf11128c0cad101058ce5e2
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-05 22:57:36 +0530

    Confgured new app, rest_framework and MySQL DB settings

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "BookApp"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,10 @@ WSGI_APPLICATION = "BookStore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "booksdb",
+        "USER": "root",
+        "PASSOWRD": "test1234"
     }
 }
 
