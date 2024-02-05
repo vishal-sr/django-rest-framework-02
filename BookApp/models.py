43b2702d496f0bb6c94cc8b838b0957d960f88e0
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-05 23:10:15 +0530

    Created Book, Author, Language models and migrated into booksdb DB/MySQL server

--- BookApp/models.py
+++ BookApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+
+class Languages(models.Model):
+    languages = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.languages}"
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=25)
+    language = models.ManyToManyField(Languages,  related_name="Languages")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    ratings = models.IntegerField(validators=[
+        MinValueValidator(0), MaxValueValidator(5)
+    ])
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="Author")
+
+    def __str__(self):
+        return f"{self.title} -- {self.author}"
--- BookStore/settings.py
+++ BookStore/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "booksdb",
         "USER": "root",
-        "PASSOWRD": "test1234"
+        "PASSWORD": "test1234"
     }
 }
 
